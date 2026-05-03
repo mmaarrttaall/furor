ca0ced4a9940345d59f188e42267ffb6a4f535c3
mmaarrttaall <martalc88@hotmail.com>
2026-05-03 21:16:32 +0200

    Update paraula.js

--- pruebas/paraula.js
+++ pruebas/paraula.js
@@ -1,1 +1,22 @@
+// ===== PROVA PARAULA =====
 
+const palabras = [
+  "AMOR", "NIT", "COR", "BALLAR", "SOL", "LLUNA",
+  "MAR", "VIDA", "BESO", "LOVE", "DANCE", "HEART"
+];
+
+let indicePalabra = 0;
+
+function cargarPruebaParaula() {
+  document.getElementById("tituloPrueba").innerText = "Prova: Cançó amb paraula";
+
+  document.getElementById("contenidoPrueba").innerHTML = `
+    <div style="font-size:28px;">Digues una cançó que contingui aquesta paraula:</div>
+    <div class="palabra-grande">${palabras[indicePalabra]}</div>
+  `;
+}
+
+function siguienteParaula() {
+  indicePalabra++;
+  if (indicePalabra >= palabras.length) indicePalabra = 0;
+}
