4fd1c51e76c50de131c9693b9bf6aa9b1e473de7
mmaarrttaall <martalc88@hotmail.com>
2026-05-09 15:30:52 +0200

    Update cascos.js

--- pruebas/cascos.js
+++ pruebas/cascos.js
@@ -2,10 +2,7 @@
 
 const cancionesCascos = [
   {
-    video: "videos/despacito.mp4"
-  },
-  {
-    video: "videos/labomba.mp4"
+    video: "videos/cascos.mp4"
   }
 ];
 
@@ -14,19 +11,7 @@ let indiceCascos = 0;
 // ===== INSTRUCCIONES =====
 
 function instruccionesCascos() {
-  document.getElementById("contenidoPrueba").innerHTML = `
-    <div class="instrucciones">
-      <p>
-        Un jugador es posa els cascos i escolta la cançó.
-        Ha de cantar-la sense dir paraules.
-        El seu equip ha d’endevinar-la.
-      </p>
-
-      <button onclick="comenzarPrueba()">Començar</button>
-    </div>
-  `;
-
-  document.getElementById("ganador").innerText = "";
+  comenzarPrueba();
 }
 
 // ===== CARGAR PRUEBA =====
@@ -36,13 +21,11 @@ function cargarPruebaCascos() {
 
   document.getElementById("contenidoPrueba").innerHTML = `
     <div class="video-cascos">
-      <video width="650" controls>
+      <video width="650" controls autoplay>
         <source src="${item.video}" type="video/mp4">
         El teu navegador no pot reproduir aquest vídeo.
       </video>
     </div>
-
-    <p>🎧 Posa't els cascos i dona-li al play</p>
   `;
 
   document.getElementById("ganador").innerText = "Esperant pulsador...";
